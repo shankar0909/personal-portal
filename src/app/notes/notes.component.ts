192dca68afbc28e31e38c8087ce372782e308cda
Shankar Sharma <shankarsharma@mac.mynetworksettings.com>
2025-08-02 14:13:46 -0400

    upgrade v 19

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-notes',
-  templateUrl: './notes.component.html',
-  styleUrls: ['./notes.component.scss']
+    selector: 'app-notes',
+    templateUrl: './notes.component.html',
+    styleUrls: ['./notes.component.scss'],
+    standalone: false
 })
 export class NotesComponent implements OnInit {
 
